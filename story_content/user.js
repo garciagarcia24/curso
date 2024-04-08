290ebfecffe123cfa6cf5609e5cb02b19033f1cd
garciagarcia24 <124152037+garciagarcia24@users.noreply.github.com>
2024-04-08 14:29:04 +0100

    jogo crianca
    jogo crianca

--- story_content/user.js
+++ story_content/user.js
@@ -2,44 +2,53 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5t3KwXtJ65a":
+      case "6aLhZLGKIS0":
         Script1();
         break;
-      case "5nbacOBNc3i":
+      case "6fVNITVyZGV":
         Script2();
         break;
-      case "6nIQhA2ui38":
+      case "5UyY24A9GsU":
         Script3();
         break;
-      case "5hBuIXhNl64":
+      case "6Tzh0fLOb9u":
         Script4();
         break;
-      case "6AjmPTWMyG3":
+      case "6MC7KwUDzwf":
         Script5();
         break;
-      case "5soM4029eT9":
+      case "6TlO1hFcmBB":
         Script6();
         break;
-      case "5ZlN7HD71s0":
+      case "6ZQbYgDj6SB":
         Script7();
         break;
-      case "6EV83vxLphL":
+      case "5ZAEGsKJ7XG":
         Script8();
         break;
-      case "5vaXAouVj6b":
+      case "5bRkLadLsum":
         Script9();
         break;
-      case "643QmrVZ3nU":
+      case "5qPLhZ6EgdN":
         Script10();
         break;
-      case "6SdAg0HDKsF":
+      case "614BXr8iVKf":
         Script11();
         break;
-      case "6m0sYbaMD1A":
+      case "6Q0YQYTgcV5":
         Script12();
         break;
-      case "5evfFE32Ei6":
+      case "68wRJyR5Eel":
         Script13();
+        break;
+      case "5aMdDqog4zZ":
+        Script14();
+        break;
+      case "5UzR4OgSPDl":
+        Script15();
+        break;
+      case "6Z1sfqbkpMz":
+        Script16();
         break;
   }
 }
@@ -316,6 +325,71 @@ target.animate([
 { scale: '1' }, { scale: `${1 + pulseAmount}` },
 { scale: '1' }, { scale: `${1 + pulseAmount}` },
 { scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script14 = function()
+{
+  const target = object('69nz5ml6w6q');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5XTX0075Rs8';
+const shakeAmount = 2;
+player.addForTriggers(
+id,
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script15 = function()
+{
+  const target = object('5z8KqZHPrIE');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6ZmPxRbvxSw';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script16 = function()
+{
+  const target = object('6ErydUURJSP');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6RGAWWwKaTj';
+const shakeAmount = 2;
+player.addForTriggers(
+id,
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
 ],
   { fill: 'forwards', duration, easing }
 )
